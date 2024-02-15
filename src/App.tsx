55dd7ead9886f644638b9d1a514793a79a430dac
Anderson Pereira <anderson.carro.95@gmail.com>
2024-02-15 00:36:24 -0300

    build 12

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Projete v4-versão build-11 atualizado</h1>
+      <h1>Projete v4-versão build-12 atualizado</h1>
     </>
   );
 }
